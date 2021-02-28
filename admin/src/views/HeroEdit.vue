124f1c72c0fd8cca46ef84fcb2dff854b747e2b1
mjy <mjy@qq.com>
2021-02-28 23:42:20 +0800

    英雄创建-添加技能

--- admin/src/views/HeroEdit.vue
+++ admin/src/views/HeroEdit.vue
@@ -3,79 +3,123 @@
 		<h1>{{id ? '编辑' : '新建'}}英雄</h1>
 		<el-form label-width="120px" 
 				@submit.native.prevent="save">
-			<!-- 新建物品 -->
-			<el-form-item label="名称">
-				<el-input v-model="model.name"></el-input>
-			</el-form-item>
-			<el-form-item label="称号">
-				<el-input v-model="model.title"></el-input>
-			</el-form-item>
-			<el-form-item label="头像">
-				<el-upload
-					class="avatar-uploader"
-					:action="$http.defaults.baseURL + '/upload'"
-					:show-file-list="false"
-					:on-success="afterUpload">
-					<!-- 如果有上传的图片就显示 该图片地址 -->
-					<img v-if="model.avatar" :src="model.avatar" class="avatar">
-					<!-- 没有上传的图片就显示 上传的图片的图标 -->
-					<i v-else class="el-icon-plus avatar-uploader-icon"></i>
-				</el-upload>
-			</el-form-item>
-			<el-form-item label="类型">
-				<!-- multiple 表示可多选下菜单中的功能 -->
-				<el-select v-model="model.categories" multiple>
-					<!-- 下拉菜单中的选项 要去关联的分类模型中查找 -->
-					<el-option v-for="item in categories" 
-					:label="item.name" :value="item._id" :key="item._id"></el-option>
-				</el-select>
-			</el-form-item>
-			<!-- 评分 -->
-			<el-form-item label="难度">
-				<!-- :max="10" 表示会显示10个星
-				show-score 在星的后面显示分数值
-			  style 样式 即设置星在页面中显示的位置 -->
-				<el-rate 
-				:max="10" show-score style="margin-top: 0.6rem;"
-				v-model="model.scores.difficult"></el-rate>
-			</el-form-item>
-			<el-form-item label="技能">
-				<el-rate 
-				:max="10" show-score style="margin-top: 0.6rem;"
-				v-model="model.scores.skills"></el-rate>
-			</el-form-item>
-			<el-form-item label="攻击">
-				<el-rate 
-				:max="10" show-score style="margin-top: 0.6rem;"
-				v-model="model.scores.attack"></el-rate>
-			</el-form-item>
-			<el-form-item label="生存">
-				<el-rate 
-				:max="10" show-score style="margin-top: 0.6rem;"
-				v-model="model.scores.survive"></el-rate>
-			</el-form-item>
-			<el-form-item label="顺风出装">
-				<el-select v-model="model.items1" multiple>
-					<el-option v-for="item in items" 
-					:label="item.name" :value="item._id" :key="item._id"></el-option>
-				</el-select>
-			</el-form-item>
-			<el-form-item label="逆风出装">
-				<el-select v-model="model.items2" multiple>
-					<el-option v-for="item in items" 
-					:label="item.name" :value="item._id" :key="item._id"></el-option>
-				</el-select>
-			</el-form-item>
-			<el-form-item label="使用技巧">
-				<el-input type="textarea" v-model="model.usageTips"></el-input>
-			</el-form-item>
-			<el-form-item label="对抗技巧">
-				<el-input type="textarea" v-model="model.battleTips"></el-input>
-			</el-form-item>
-			<el-form-item label="团战思路">
-				<el-input type="textarea" v-model="model.teamTips"></el-input>
-			</el-form-item>
-			<el-form-item>
+			<!-- value="basic" 表示默认显示对应下的name中的参数 -->
+			<el-tabs type="border-card" value="basic">
+				<el-tab-pane label="基本信息" name="basic">
+					<!-- 新建英雄 -->
+					<el-form-item label="名称">
+						<el-input v-model="model.name"></el-input>
+					</el-form-item>
+					<el-form-item label="称号">
+						<el-input v-model="model.title"></el-input>
+					</el-form-item>
+					<el-form-item label="头像">
+						<el-upload
+							class="avatar-uploader"
+							:action="$http.defaults.baseURL + '/upload'"
+							:show-file-list="false"
+							:on-success="afterUpload">
+							<!-- 如果有上传的图片就显示 该图片地址 -->
+							<img v-if="model.avatar" :src="model.avatar" class="avatar">
+							<!-- 没有上传的图片就显示 上传的图片的图标 -->
+							<i v-else class="el-icon-plus avatar-uploader-icon"></i>
+						</el-upload>
+					</el-form-item>
+					<el-form-item label="类型">
+						<!-- multiple 表示可多选下菜单中的功能 -->
+						<el-select v-model="model.categories" multiple>
+							<!-- 下拉菜单中的选项 要去关联的分类模型中查找 -->
+							<el-option v-for="item in categories" 
+							:label="item.name" :value="item._id" :key="item._id"></el-option>
+						</el-select>
+					</el-form-item>
+					<!-- 评分 -->
+					<el-form-item label="难度">
+						<!-- :max="10" 表示会显示10个星
+						show-score 在星的后面显示分数值
+						style 样式 即设置星在页面中显示的位置 -->
+						<el-rate 
+						:max="10" show-score style="margin-top: 0.6rem;"
+						v-model="model.scores.difficult"></el-rate>
+					</el-form-item>
+					<el-form-item label="技能">
+						<el-rate 
+						:max="10" show-score style="margin-top: 0.6rem;"
+						v-model="model.scores.skills"></el-rate>
+					</el-form-item>
+					<el-form-item label="攻击">
+						<el-rate 
+						:max="10" show-score style="margin-top: 0.6rem;"
+						v-model="model.scores.attack"></el-rate>
+					</el-form-item>
+					<el-form-item label="生存">
+						<el-rate 
+						:max="10" show-score style="margin-top: 0.6rem;"
+						v-model="model.scores.survive"></el-rate>
+					</el-form-item>
+					<el-form-item label="顺风出装">
+						<el-select v-model="model.items1" multiple>
+							<el-option v-for="item in items" 
+							:label="item.name" :value="item._id" :key="item._id"></el-option>
+						</el-select>
+					</el-form-item>
+					<el-form-item label="逆风出装">
+						<el-select v-model="model.items2" multiple>
+							<el-option v-for="item in items" 
+							:label="item.name" :value="item._id" :key="item._id"></el-option>
+						</el-select>
+					</el-form-item>
+					<el-form-item label="使用技巧">
+						<el-input type="textarea" v-model="model.usageTips"></el-input>
+					</el-form-item>
+					<el-form-item label="对抗技巧">
+						<el-input type="textarea" v-model="model.battleTips"></el-input>
+					</el-form-item>
+					<el-form-item label="团战思路">
+						<el-input type="textarea" v-model="model.teamTips"></el-input>
+					</el-form-item>
+				</el-tab-pane>
+				<el-tab-pane label="技能" name="skills">
+					<!-- type="text" 表示按钮的样式是文字形式 即没有边框
+						el-icon-plus 表示 + 号小图标 -->
+					<el-button size="small" @click="model.skills.push({})"><i class="el-icon-plus"></i> 添加技能</el-button>
+					<!-- 类型是flax布局 样式是换行 -->
+					<el-row type="flex" style="flex-wrap: wrap;">
+						<!-- :md="12"表示把一行分为两份
+						model.skills 表示该英雄的所有技能 -->
+						<el-col :md="12" v-for="(item, i) in model.skills"
+							:key="i">
+							<el-form-item label="名称">
+								<el-input v-model="item.name"></el-input>
+							</el-form-item>
+							<el-form-item label="图标">
+								<!-- :on-success即上传成功后做的事情 接收一个函数
+								"res => $set(item, 'icon', res.url)" 把接收到的url赋值给icon -->
+								<el-upload
+									class="avatar-uploader"
+									:action="$http.defaults.baseURL + '/upload'"
+									:show-file-list="false"
+									:on-success="res => $set(item, 'icon', res.url)">
+									<img v-if="item.icon" :src="item.icon" class="avatar">
+									<i v-else class="el-icon-plus avatar-uploader-icon"></i>
+								</el-upload>
+							</el-form-item>
+							<el-form-item label="描述">
+								<el-input v-model="item.description" type="textarea"></el-input>
+							</el-form-item>
+							<el-form-item label="小提示">
+								<el-input v-model="item.tips" type="textarea"></el-input>
+							</el-form-item>
+							<el-form-item>
+								<!-- splice 删除数组中的元素 -->
+								<el-button size="small" type="danger"
+								@click="model.skills.splice(i, 1)">删除</el-button>
+							</el-form-item>
+						</el-col>
+					</el-row>
+				</el-tab-pane>
+			</el-tabs>
+			<el-form-item style="margin-top: 1rem;">
 				<el-button type="primary" native-type="submit">保存</el-button>
 			</el-form-item>
 		</el-form>
@@ -91,6 +135,8 @@
 				categories: [], // 存放英雄分类数据 下拉菜单中数据
 				items: [], // 存放英雄出装数据 下拉菜单中数据
 				model: { // 新建英雄是存放的数据
+					// 存放英雄的技能 否则在新建英雄的技能页面会报错 即找不到push
+					skills: [], 
 					name: '', // 名称
 					avatar: '', // 头像
 					scores: { // 英雄评分
@@ -159,14 +205,14 @@
   .avatar-uploader-icon {
     font-size: 28px;
     color: #8c939d;
-    width: 178px;
-    height: 178px;
-    line-height: 178px;
+    width: 5rem;
+    height: 5rem;
+    line-height: 5rem;
     text-align: center;
   }
   .avatar {
-    width: 178px;
-    height: 178px;
+    width: 5rem;
+    height: 5rem;
     display: block;
   }
 </style>
