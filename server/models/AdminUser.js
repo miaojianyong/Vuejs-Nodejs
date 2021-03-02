4a9fa72206d7f365b6b1cb157b3e67fae896cffb
mjy <mjy@qq.com>
2021-03-02 21:34:09 +0800

    密码加密处理

--- server/models/AdminUser.js
+++ server/models/AdminUser.js
@@ -7,7 +7,13 @@ const schema = new mongoose.Schema({
     type: String,
   },
   password: { // 密码
-    type: String
+    type: String,
+    select: false, // 表示经过加密处理的密码不在前端显示
+    // val 表示用户填写的密码
+    set(val) { // 对密码加密后在存储到数据库
+      // 导入bcrypt密码加密模块 10加密指数即加密程度
+      return require('bcrypt').hashSync(val, 10)
+    }
   }
 })
 // 导出物品模型
