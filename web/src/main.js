c1590e2432a15029bd3a1d6f2696d417f20baada
mjy <mjy@qq.com>
2021-03-07 19:49:58 +0800

    轮播图插件的使用、scss文件的分离

--- web/src/main.js
+++ web/src/main.js
@@ -5,9 +5,16 @@ import App from './App.vue'
 Vue.config.productionTip = false
 
 /* 引用公共样式文件 */
-import './style.scss';
-
+import './assets/scss/style.scss';
+// 引入路由文件
 import router from './router'
+// 引入制作轮播图插件 在此处引用表示全局引用
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+// 引入轮播图插件的对应样式
+import 'swiper/dist/css/swiper.css'
+// 使用轮播图插件
+Vue.use(VueAwesomeSwiper, /* { default options with global component } */)
+
 new Vue({
   router,
   render: h => h(App)
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,13 +1,46 @@
 <template>
   <div> 
-    首页
+    <swiper :options="swiperOptions">
+      <swiper-slide>
+        <img class="w-100" src="../assets/images/1.jpeg" alt="">
+      </swiper-slide>
+      <swiper-slide>
+        <img class="w-100" src="../assets/images/2.jpeg" alt="">
+      </swiper-slide>
+      <swiper-slide>
+        <img class="w-100" src="../assets/images/3.jpeg" alt="">
+      </swiper-slide>
+      <!-- 添加pagination-home类 即为首页幻灯片的 避免类名冲突 -->
+      <div class="swiper-pagination pagination-home text-right px-3 pd-1" 
+      slot="pagination"></div>
+    </swiper>
   </div>
 </template> 
 
-<script>
+<script> 
   export default {
-
+    data() {
+      return {
+        swiperOptions: {
+          pagination: { // 上述定义的首页幻灯片类
+            el: '.pagination-home'
+          }
+        }
+      }
+    }
   }
 </script>
 
-<style></style>
+<style lang="scss"> /* 即声明样式是使用的scss编写 */
+@import '../assets/scss/variables.scss'; /* 引入样式文件 */
+  .pagination-home {
+    .swiper-pagination-bullet {
+      opacity: 1; /* 去掉半透明效果 */
+      border-radius: 0.1538rem; /* 设置圆角 */
+      background: map-get($colors, 'white');
+      &.swiper-pagination-bullet-active {
+        background: map-get($colors, 'info');
+      }
+    }
+  }
+</style>
